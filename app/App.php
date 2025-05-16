922c131d49f2272869bbf7e5872fc98435cc024a
mariam <mariamemampp00@gmail.com>
2025-05-16 16:42:41 +0300

    make function to calculate the total

--- app/App.php
+++ app/App.php
@@ -48,4 +48,20 @@ function extractTransaction(array $transactionsRow):array
     ];
 
 }
+
+function calculateTotal(array $transactions):array
+{
+    $totals = ['netTotal'=> 0 , 'totalIncome'=>0, 'totalExpense'=>0];
+    foreach($transactions as $transaction){
+        $totals['netTotal'] += $transaction['amount'];
+        if($transaction['amount'] > 0){
+            $totals['totalIncome'] += $transaction['amount'];
+        } else{
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+            
+    }
+        
+    return $totals;
+}
  
