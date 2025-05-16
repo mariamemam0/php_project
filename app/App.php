c2ebbea6cc000276860c3ff18f7bfcd50648ff83
mariam <mariamemampp00@gmail.com>
2025-05-16 12:57:07 +0300

    make functions to get the files and read the data of the files

--- app/App.php
+++ app/App.php
@@ -4,12 +4,32 @@ declare(strict_types = 1);
 
 // Your Code
 
-function getTransactionFiles():array
+function getTransactionFiles(string $dirPath):array
 {
     $files = [];
-    foreach(scandir(FILES_PATH)as $file)
+    foreach(scandir($dirPath)as $file)
     {
+        if(is_dir($file)){
+            continue;
+        }
+        $files[]= $dirPath . $file;
         var_dump($file);
     }
+    return $files;
+}
+
+function getTransaction(string $filename):array
+{
+    if(! file_exists($filename)){
+        trigger_error("file".$filename."does not exist" , E_USER_ERROR);
+    }
+
+    $file = fopen($filename , "r");
+    fgetcsv($file);
+    $transactions = [];
+    while(($transaction = fgetcsv($file)) !== false ){
+        $transactions[] = $transaction;
+    }
+    return $transactions;
 }
  
