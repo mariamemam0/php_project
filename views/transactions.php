2d8a9328f267f254fbfbf58567a3b7b550187848
mariam <mariamemampp00@gmail.com>
2025-05-16 16:58:25 +0300

    controle the color of the amount

--- views/transactions.php
+++ views/transactions.php
@@ -38,10 +38,22 @@
                  <?php if (! empty($transactions)): ?>
                     <?php foreach ($transactions as $transaction ):?>
                         <tr>
-                            <td><?= $transaction['date']?></td>
+                            <td><?=  formatDate($transaction['date'])?></td>
                             <td><?= $transaction['check_number']?></td>
                             <td><?= $transaction['description']?></td>
-                            <td><?= formatDollarAmount($transaction['amount'])?></td>
+                            <td>
+                                <?php if($transaction['amount']<0): ?>
+                                    <span style="color: red;">
+                                <?= formatDollarAmount($transaction['amount'])?>
+                                    </span>
+                                <?php elseif ($transaction['amount'] > 0):?>
+                                    <span style="color: green;">
+                                <?= formatDollarAmount($transaction['amount'])?>
+                                    </span>
+                                <?php else: ?>
+                                   <?= formatDollarAmount($transaction['amount'])?>
+                                   <?php endif ?>   
+                            </td>
                         </tr>
                     <?php endforeach ?>
                  <?php endif ?>   
